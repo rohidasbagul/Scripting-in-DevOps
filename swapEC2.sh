ad42b9874c20d329725a513c1dd0801e49795717
Rohidas Bagul <rohidasbagul@gmail.com>
2021-12-07 17:08:23 +0530

    Update swapEC2.sh

--- swapEC2.sh
+++ swapEC2.sh
@@ -2,10 +2,10 @@
 #USE_ME
 #ROHIDAS_BAGUL
 
-dd if=/dev/zero of=/swap bs=1024 count=2097152 #create 2GB [rohidasbagul@e-zest.in]
+dd if=/dev/zero of=/swap bs=1024 count=2097152 #create 2GB
 sleep 2
 mkswap /swap && chown root. /swap && chmod 0600 /swap && swapon /swap
 echo /swap swap swap defaults 0 0 >> /etc/fstab
-echo vm.swappiness = 0 >> /etc/sysctl.conf && sysctl -p
+echo vm.swappiness = 60 >> /etc/sysctl.conf && sysctl -p
 #EC2-swap space create.
 echo "swap sapce create completed..."
